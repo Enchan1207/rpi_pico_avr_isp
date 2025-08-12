6d6ea83bb34156561d42c98719ea3f89a85603e1
enchan <enchant0130@gmail.com>
2025-08-12 22:40:26 +0900

    chore: add extern-C

--- include/core/calc.h
+++ include/core/calc.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * @brief 二つの値を加算する
  *
@@ -11,5 +15,9 @@
  * @return uint8_t
  */
 uint8_t add(uint8_t lhs, uint8_t rhs);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // PARSER_H
