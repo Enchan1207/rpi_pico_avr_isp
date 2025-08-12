96c107857f5623e2d254a183411f6b2f88d085cc
enchan <enchant0130@gmail.com>
2025-08-12 22:32:43 +0900

    feat: #2 コアライブラリを追加

---
+++ include/core/calc.h
@@ -0,0 +1,15 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <stdint.h>
+
+/**
+ * @brief 二つの値を加算する
+ *
+ * @param lhs
+ * @param rhs
+ * @return uint8_t
+ */
+uint8_t add(uint8_t lhs, uint8_t rhs);
+
+#endif  // PARSER_H
