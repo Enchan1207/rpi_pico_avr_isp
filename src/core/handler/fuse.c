f56cbfd7bda402ec765387d777cc4696ca283e75
enchan <enchant0130@gmail.com>
2025-08-16 15:30:39 +0900

    feat: #6 フューズコマンドを実装させた

--- src/core/handler/fuse.c
+++ src/core/handler/fuse.c
@@ -1,25 +1,58 @@
 #include "handler_private.h"
 
 void handleProgFuse(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    uint8_t fuse_low = parserCtx->arguments[0];
+    uint8_t fuse_high = parserCtx->arguments[1];
+
+    handlerCtx->transferFunc(0xAC, 0xA0, 0x00, fuse_low);
+    handlerCtx->transferFunc(0xAC, 0xA8, 0x00, fuse_high);
+
+    const uint8_t response[] = {STK500_RESP_IN_SYNC, STK500_RESP_OK};
+    handlerCtx->writeResponse(response, sizeof(response));
 }
 
 void handleReadFuse(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    uint8_t fuse_low = handlerCtx->transferFunc(0x50, 0x00, 0x00, 0x00);
+    uint8_t fuse_high = handlerCtx->transferFunc(0x58, 0x08, 0x00, 0x00);
+
+    const uint8_t response[] = {STK500_RESP_IN_SYNC, fuse_low, fuse_high, STK500_RESP_OK};
+    handlerCtx->writeResponse(response, sizeof(response));
 }
 
 void handleProgFuseExt(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    uint8_t fuse_low = parserCtx->arguments[0];
+    uint8_t fuse_high = parserCtx->arguments[1];
+    uint8_t fuse_ext = parserCtx->arguments[2];
+
+    handlerCtx->transferFunc(0xAC, 0xA0, 0x00, fuse_low);
+    handlerCtx->transferFunc(0xAC, 0xA8, 0x00, fuse_high);
+    handlerCtx->transferFunc(0xAC, 0xA4, 0x00, fuse_ext);
+
+    const uint8_t response[] = {STK500_RESP_IN_SYNC, STK500_RESP_OK};
+    handlerCtx->writeResponse(response, sizeof(response));
 }
 
 void handleReadFuseExt(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    uint8_t fuse_low = handlerCtx->transferFunc(0x50, 0x00, 0x00, 0x00);
+    uint8_t fuse_high = handlerCtx->transferFunc(0x58, 0x08, 0x00, 0x00);
+    uint8_t fuse_ext = handlerCtx->transferFunc(0x50, 0x08, 0x00, 0x00);
+
+    const uint8_t response[] = {STK500_RESP_IN_SYNC, fuse_low, fuse_high, fuse_ext, STK500_RESP_OK};
+    handlerCtx->writeResponse(response, sizeof(response));
 }
 
 void handleProgLock(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    uint8_t lock = parserCtx->arguments[0];
+
+    handlerCtx->transferFunc(0xAC, 0xE0, 0x00, lock);
+
+    const uint8_t response[] = {STK500_RESP_IN_SYNC, STK500_RESP_OK};
+    handlerCtx->writeResponse(response, sizeof(response));
 }
 
 void handleReadLock(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    uint8_t lock = handlerCtx->transferFunc(0x58, 0x00, 0x00, 0x00);
+
+    const uint8_t response[] = {STK500_RESP_IN_SYNC, lock, STK500_RESP_OK};
+    handlerCtx->writeResponse(response, sizeof(response));
 }
