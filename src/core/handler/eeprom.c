fba4210dd40edc35ddc2bc334e5a76da819fa1dd
enchan <enchant0130@gmail.com>
2025-08-16 23:01:27 +0900

    feat: #6 EEPROMアクセス関数を実装させた

--- src/core/handler/eeprom.c
+++ src/core/handler/eeprom.c
@@ -1,9 +1,28 @@
 #include "handler_private.h"
 
 void handleProgData(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    uint8_t data = parserCtx->arguments[0];
+    uint16_t address = handlerCtx->currentAddress;
+
+    uint8_t addressHigh = (address >> 8) & 0xFF;
+    uint8_t addressLow = address & 0xFF;
+
+    handlerCtx->transferFunc(0xC0, addressHigh, addressLow, data);
+    
+    handlerCtx->sleepFunc(11);
+
+    const uint8_t response[] = {STK500_RESP_IN_SYNC, STK500_RESP_OK};
+    handlerCtx->writeResponse(response, sizeof(response));
 }
 
 void handleReadData(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    uint16_t address = handlerCtx->currentAddress;
+
+    uint8_t addressHigh = (address >> 8) & 0xFF;
+    uint8_t addressLow = address & 0xFF;
+
+    uint8_t data = handlerCtx->transferFunc(0xA0, addressHigh, addressLow, 0x00);
+
+    const uint8_t response[] = {STK500_RESP_IN_SYNC, data, STK500_RESP_OK};
+    handlerCtx->writeResponse(response, sizeof(response));
 }
