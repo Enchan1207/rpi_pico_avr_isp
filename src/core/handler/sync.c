b5e57a47597dd102f6d568a19763cfc97a42b81f
enchan <enchant0130@gmail.com>
2025-08-16 11:54:06 +0900

    fix: テストケースがパスするようロジックを修正

--- src/core/handler/sync.c
+++ src/core/handler/sync.c
@@ -1,9 +1,19 @@
+#include <string.h>
+
 #include "handler_private.h"
 
 void handleGetSync(parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    const uint8_t response[] = {STK500_RESP_IN_SYNC, STK500_RESP_OK};
+    handlerCtx->writeResponse(response, sizeof(response));
 }
 
 void handleGetSignOn(parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    const uint8_t signOnMessage[] = "AVR STK";
+
+    uint8_t response[sizeof(signOnMessage) + 1] = {0};
+    response[0] = STK500_RESP_IN_SYNC;
+    response[sizeof(response) - 1] = STK500_RESP_OK;
+    memcpy(response + 1, signOnMessage, sizeof(signOnMessage) - 1);
+
+    handlerCtx->writeResponse(response, sizeof(response));
 }
