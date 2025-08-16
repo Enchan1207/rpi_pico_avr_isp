23c985ca33e610db792210ca1466870e26d2585a
enchan <enchant0130@gmail.com>
2025-08-16 17:24:28 +0900

    feat: #6 set/get parameter を実装させた

--- src/core/handler/device.c
+++ src/core/handler/device.c
@@ -1,11 +1,86 @@
+#include "core/stk500_parameters.h"
 #include "handler_private.h"
 
 void handleSetParameter(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    uint8_t parameter = parserCtx->arguments[0];
+
+    switch (parameter) {
+        case PARM_STK_HW_VER:
+        case PARM_STK_SW_MAJOR:
+        case PARM_STK_SW_MINOR:
+        case PARM_STK_BUFSIZEL:
+        case PARM_STK_BUFSIZEH:
+        case PARAM_STK500_TOPCARD_DETECT: {
+            const uint8_t failedResponse[] = {STK500_RESP_IN_SYNC, parameter, STK500_RESP_FAILED};
+            handlerCtx->writeResponse(failedResponse, sizeof(failedResponse));
+            break;
+        }
+
+        case PARM_STK_LEDS:
+        case PARM_STK_VTARGET:
+        case PARM_STK_VADJUST:
+        case PARM_STK_OSC_PSCALE:
+        case PARM_STK_OSC_CMATCH:
+        case PARM_STK_SCK_DURATION: {
+            const uint8_t okResponse[] = {STK500_RESP_IN_SYNC, STK500_RESP_OK};
+            handlerCtx->writeResponse(okResponse, sizeof(okResponse));
+            break;
+        }
+
+        default: {
+            const uint8_t unknownResponse[] = {STK500_RESP_IN_SYNC, parameter, STK500_RESP_FAILED};
+            handlerCtx->writeResponse(unknownResponse, sizeof(unknownResponse));
+            break;
+        }
+    }
 }
 
 void handleGetParameter(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    uint8_t parameter = parserCtx->arguments[0];
+    uint8_t value;
+    uint8_t status;
+
+    switch (parameter) {
+        case PARM_STK_HW_VER:
+            value = STK500_HW_VERSION;
+            status = STK500_RESP_OK;
+            break;
+
+        case PARM_STK_SW_MAJOR:
+            value = STK500_SW_MAJOR;
+            status = STK500_RESP_OK;
+            break;
+
+        case PARM_STK_SW_MINOR:
+            value = STK500_SW_MINOR;
+            status = STK500_RESP_OK;
+            break;
+
+        case PARAM_STK500_TOPCARD_DETECT:
+            value = 0x03;
+            status = STK500_RESP_OK;
+            break;
+
+        case PARM_STK_BUFSIZEL:
+        case PARM_STK_BUFSIZEH:
+        case PARM_STK_LEDS:
+        case PARM_STK_VTARGET:
+        case PARM_STK_VADJUST:
+        case PARM_STK_OSC_PSCALE:
+        case PARM_STK_OSC_CMATCH:
+        case PARM_STK_SCK_DURATION:
+            value = 0x00;
+            status = STK500_RESP_OK;
+            break;
+
+        default:
+            value = parameter;
+            status = STK500_RESP_FAILED;
+            break;
+    }
+
+    const uint8_t response[] = {STK500_RESP_IN_SYNC, value, status};
+    handlerCtx->writeResponse(response, sizeof(response));
 }
 
 void handleSetDevice(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
--- tests/handler/test_device.cpp
+++ tests/handler/test_device.cpp
@@ -52,4 +52,98 @@ TEST_F(DeviceHandlerTest, HandleReadOscCalExtCommand) {
     EXPECT_EQ(capturedResponse, expectedResponse);
 }
 
+TEST_F(DeviceHandlerTest, HandleGetParameterHwVersion) {
+    parserCtx.command = STK500_CMD_GET_PARAMETER;
+    parserCtx.expectedArgumentsLength = 1;
+    parserCtx.receivedArgumentsLength = 1;
+    parserCtx.arguments[0] = 0x80;  // PARM_STK_HW_VER
+
+    handleCommand(&parserCtx, &handlerCtx);
+
+    std::vector<uint8_t> expectedResponse = {STK500_RESP_IN_SYNC, 0x02, STK500_RESP_OK};
+    ASSERT_EQ(capturedResponse.size(), expectedResponse.size());
+    EXPECT_EQ(capturedResponse, expectedResponse);
+}
+
+TEST_F(DeviceHandlerTest, HandleGetParameterSwMajor) {
+    parserCtx.command = STK500_CMD_GET_PARAMETER;
+    parserCtx.expectedArgumentsLength = 1;
+    parserCtx.receivedArgumentsLength = 1;
+    parserCtx.arguments[0] = 0x81;  // PARM_STK_SW_MAJOR
+
+    handleCommand(&parserCtx, &handlerCtx);
+
+    std::vector<uint8_t> expectedResponse = {STK500_RESP_IN_SYNC, 0x01, STK500_RESP_OK};
+    ASSERT_EQ(capturedResponse.size(), expectedResponse.size());
+    EXPECT_EQ(capturedResponse, expectedResponse);
+}
+
+TEST_F(DeviceHandlerTest, HandleGetParameterSwMinor) {
+    parserCtx.command = STK500_CMD_GET_PARAMETER;
+    parserCtx.expectedArgumentsLength = 1;
+    parserCtx.receivedArgumentsLength = 1;
+    parserCtx.arguments[0] = 0x82;  // PARM_STK_SW_MINOR
+
+    handleCommand(&parserCtx, &handlerCtx);
+
+    std::vector<uint8_t> expectedResponse = {STK500_RESP_IN_SYNC, 0x00, STK500_RESP_OK};
+    ASSERT_EQ(capturedResponse.size(), expectedResponse.size());
+    EXPECT_EQ(capturedResponse, expectedResponse);
+}
+
+TEST_F(DeviceHandlerTest, HandleGetParameterUnknown) {
+    parserCtx.command = STK500_CMD_GET_PARAMETER;
+    parserCtx.expectedArgumentsLength = 1;
+    parserCtx.receivedArgumentsLength = 1;
+    parserCtx.arguments[0] = 0xFF;  // 不明なパラメータ
+
+    handleCommand(&parserCtx, &handlerCtx);
+
+    std::vector<uint8_t> expectedResponse = {STK500_RESP_IN_SYNC, 0xFF, STK500_RESP_FAILED};
+    ASSERT_EQ(capturedResponse.size(), expectedResponse.size());
+    EXPECT_EQ(capturedResponse, expectedResponse);
+}
+
+TEST_F(DeviceHandlerTest, HandleSetParameterReadOnly) {
+    parserCtx.command = STK500_CMD_SET_PARAMETER;
+    parserCtx.expectedArgumentsLength = 2;
+    parserCtx.receivedArgumentsLength = 2;
+    parserCtx.arguments[0] = 0x80;  // PARM_STK_HW_VER (読み取り専用)
+    parserCtx.arguments[1] = 0x05;
+
+    handleCommand(&parserCtx, &handlerCtx);
+
+    std::vector<uint8_t> expectedResponse = {STK500_RESP_IN_SYNC, 0x80, STK500_RESP_FAILED};
+    ASSERT_EQ(capturedResponse.size(), expectedResponse.size());
+    EXPECT_EQ(capturedResponse, expectedResponse);
+}
+
+TEST_F(DeviceHandlerTest, HandleSetParameterWritable) {
+    parserCtx.command = STK500_CMD_SET_PARAMETER;
+    parserCtx.expectedArgumentsLength = 2;
+    parserCtx.receivedArgumentsLength = 2;
+    parserCtx.arguments[0] = 0x83;  // PARM_STK_LEDS (読み書き可能)
+    parserCtx.arguments[1] = 0x01;
+
+    handleCommand(&parserCtx, &handlerCtx);
+
+    std::vector<uint8_t> expectedResponse = {STK500_RESP_IN_SYNC, STK500_RESP_OK};
+    ASSERT_EQ(capturedResponse.size(), expectedResponse.size());
+    EXPECT_EQ(capturedResponse, expectedResponse);
+}
+
+TEST_F(DeviceHandlerTest, HandleSetParameterUnknown) {
+    parserCtx.command = STK500_CMD_SET_PARAMETER;
+    parserCtx.expectedArgumentsLength = 2;
+    parserCtx.receivedArgumentsLength = 2;
+    parserCtx.arguments[0] = 0xFF;  // 不明なパラメータ
+    parserCtx.arguments[1] = 0x01;
+
+    handleCommand(&parserCtx, &handlerCtx);
+
+    std::vector<uint8_t> expectedResponse = {STK500_RESP_IN_SYNC, 0xFF, STK500_RESP_FAILED};
+    ASSERT_EQ(capturedResponse.size(), expectedResponse.size());
+    EXPECT_EQ(capturedResponse, expectedResponse);
+}
+
 }  // namespace
