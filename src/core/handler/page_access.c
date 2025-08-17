95899df22df9a051e66d1ee398162003873c763d
enchan <enchant0130@gmail.com>
2025-08-17 15:27:23 +0900

    feat: フラッシュメモリおよびEEPROMのページ読み出し機能を実装

--- src/core/handler/page_access.c
+++ src/core/handler/page_access.c
@@ -1,7 +1,7 @@
 #include "handler_private.h"
 
 void handleProgPage(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // const uint16_t numberOfWords = parserCtx->arguments[0] << 8 | parserCtx->arguments[1];
+    // const uint16_t numberOfData = parserCtx->arguments[0] << 8 | parserCtx->arguments[1];
     // const char memoryType = parserCtx->arguments[2];
     // uint8_t* data = parserCtx->arguments + 3;
 
@@ -11,17 +11,45 @@ void handleProgPage(const parser_context_t* parserCtx, handler_context_t* handle
 }
 
 void handleReadPage(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    const uint16_t numberOfWords = parserCtx->arguments[0] << 8 | parserCtx->arguments[1];
-    // const char memoryType = parserCtx->arguments[2];
+    const size_t numberOfBytes = parserCtx->arguments[0] << 8 | parserCtx->arguments[1];
+    uint8_t response[numberOfBytes + 2];
 
-    uint8_t response[numberOfWords];
-    size_t index = 0;
+    const char memoryType = parserCtx->arguments[2];
 
-    response[index++] = STK500_RESP_IN_SYNC;
-    for (size_t i = 0; i < numberOfWords; i++) {
-        // TODO: フラッシュの各ワードを読み込んで代入
-        response[index++] = 0x00;
+    // 不明なメモリタイプ
+    if (memoryType != 'E' && memoryType != 'F') {
+        handlerCtx->writeResponse((uint8_t[]){STK500_RESP_NO_SYNC}, 1);
+        return;
     }
+
+    // NOTE: フラッシュメモリの場合、currentAddressがワードアドレスとなるため、
+    // NOTE: ループ回数は指定されたデータ数の半分でよい
+    const size_t loopCount = memoryType == 'F' ? numberOfBytes >> 1 : numberOfBytes;
+    size_t index = 0;
+    response[index++] = STK500_RESP_IN_SYNC;
+
+    for (size_t i = 0; i < loopCount; i++) {
+        const uint16_t base = handlerCtx->currentAddress;
+
+        if (memoryType == 'E') {
+            const uint8_t data = handlerCtx->transfer(0xA0, (uint8_t)(base >> 8), (uint8_t)(base & 0xFF), 0x00);
+
+            handlerCtx->sleep(11);
+
+            response[index++] = data;
+        } else {
+            const uint8_t dataLow = handlerCtx->transfer(0x20, (uint8_t)(base >> 8), (uint8_t)(base & 0xFF), 0x00);
+            const uint8_t dataHigh = handlerCtx->transfer(0x28, (uint8_t)(base >> 8), (uint8_t)(base & 0xFF), 0x00);
+
+            handlerCtx->sleep(14);
+
+            response[index++] = dataLow;
+            response[index++] = dataHigh;
+        }
+
+        handlerCtx->currentAddress = base + 1;
+    }
+
     response[index++] = STK500_RESP_OK;
 
     handlerCtx->writeResponse(response, index);
