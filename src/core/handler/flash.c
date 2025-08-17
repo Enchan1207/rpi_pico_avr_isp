6615003aa6a593d5f8df3e0cffe5be869239a69e
enchan <enchant0130@gmail.com>
2025-08-17 09:41:59 +0900

    feat: #6 Flashのワード単位アクセスを実装させた

--- src/core/handler/flash.c
+++ src/core/handler/flash.c
@@ -1,9 +1,35 @@
 #include "handler_private.h"
 
 void handleProgFlash(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    uint8_t flashLow = parserCtx->arguments[0];
+    uint8_t flashHigh = parserCtx->arguments[1];
+    uint16_t address = handlerCtx->currentAddress;
+
+    uint8_t addressHigh = (address >> 8) & 0xFF;
+    uint8_t addressLow = address & 0xFF;
+
+    handlerCtx->transferFunc(0x40, addressHigh, addressLow, flashLow);
+    handlerCtx->transferFunc(0x48, addressHigh, addressLow, flashHigh);
+
+    handlerCtx->sleepFunc(14);
+
+    handlerCtx->currentAddress++;
+
+    const uint8_t response[] = {STK500_RESP_IN_SYNC, STK500_RESP_OK};
+    handlerCtx->writeResponse(response, sizeof(response));
 }
 
 void handleReadFlash(const parser_context_t* parserCtx, handler_context_t* handlerCtx) {
-    // TODO: 実装
+    uint16_t address = handlerCtx->currentAddress;
+
+    uint8_t addressHigh = (address >> 8) & 0xFF;
+    uint8_t addressLow = address & 0xFF;
+
+    uint8_t flashLow = handlerCtx->transferFunc(0x20, addressHigh, addressLow, 0x00);
+    uint8_t flashHigh = handlerCtx->transferFunc(0x28, addressHigh, addressLow, 0x00);
+
+    handlerCtx->currentAddress++;
+
+    const uint8_t response[] = {STK500_RESP_IN_SYNC, flashLow, flashHigh, STK500_RESP_OK};
+    handlerCtx->writeResponse(response, sizeof(response));
 }
